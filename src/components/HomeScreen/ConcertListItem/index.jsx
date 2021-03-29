5a744e91324765c65e7ac8ce2f60ba73d6c9b54f
Sean Leonard <seanleonardsap@gmail.com>
2021-03-29 19:29:12 +0100

    fixed styles - for prototype on one device quickly

--- src/components/HomeScreen/ConcertListItem/index.jsx
+++ src/components/HomeScreen/ConcertListItem/index.jsx
@@ -1,39 +1,56 @@
-import React from 'react';
-import { TouchableHighlight } from 'react-native-gesture-handler';
-import { Text, View } from 'react-native';
+import React from "react";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import { Text, View, StyleSheet } from "react-native";
 
-export const ConcertListItem = ({navigation, item}) => {
-    
-    return (
+const styles = StyleSheet.create({
+  item: {
+    //padding: 10,
+    height: 44,
+    borderBottomColor: "lightbluesteel",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
 
-      <TouchableHighlight onPress={()=> navigation.navigate("Details", {item})}>
+  tab: {
+    width: 68,
+    backgroundColor: "black",
+    padding: 12,
+    position: "absolute",
+    left: 0,
+  },
 
-			<View>
+  tabCenter: {
+    width: 272,
+    padding: 12,
+    position: "absolute",
+    right: 0,
+  },
 
-				<View>
-				
-					<Text>
+  tabText: {
+    color: "white",
+    fontSize: 14,
+    whiteSpace: "nowrap",
+  },
+});
 
-						{item.date.toLocaleDateString("en-us", {day: "2-digit", month:"short"})}
+export const ConcertListItem = ({ navigation, item }) => {
+  return (
+    <TouchableHighlight
+      onPress={() => navigation.navigate("Details", { item })}
+    >
+      <View style={styles.item}>
+        <View style={styles.tab}>
+          <Text style={styles.tabText}>
+            {item.date.toLocaleDateString("en-us", {
+              day: "2-digit",
+              month: "short",
+            })}
+          </Text>
+        </View>
 
-					</Text>
-
-				</View>
-
-				<View>
-
-					<Text>
-
-						{item.name}
-
-					</Text>
-
-				</View>
-			
-			</View>
-
-		</TouchableHighlight>
-
-    )
-
-}
+        <View style={styles.tabCenter}>
+          <Text>{item.name}</Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+};
--- src/components/HomeScreen/index.jsx
+++ src/components/HomeScreen/index.jsx
@@ -1,42 +1,42 @@
-import React, { useRef } from 'react';
-import { ScrollView } from 'react-native-gesture-handler';
-import { Text, View, SectionList, } from 'react-native';
+import React, { useRef } from "react";
+import { ScrollView } from "react-native-gesture-handler";
+import { Text, View, SectionList, StyleSheet } from "react-native";
 
-import { defaultState } from '../../config';
-import { ConcertListItem } from './ConcertListItem';
+import { defaultState } from "../../config";
+import { ConcertListItem } from "./ConcertListItem";
 
+export const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <SectionList
+          sections={defaultState}
+          renderItem={({ item }) => (
+            <ConcertListItem item={item} navigation={navigation} />
+          )}
+          renderSectionHeader={({ section }) => (
+            <Text style={styles.sectionHeader}>{section.title}</Text>
+          )}
+        />
+      </ScrollView>
 
-export const HomeScreen = ({navigation})=>{
+      <Text>(C)2020 Globoticket</Text>
+    </View>
+  );
+};
 
-    return (
-        <View>
-
-				  <ScrollView>
-
-                	<SectionList
-	                    sections={defaultState} 
-						renderItem={({item}) => (
-						
-							<ConcertListItem item={item} navigation={navigation}/> 
-						
-						)}
-                  		renderSectionHeader={({section}) => (
-
-                      		<Text>
-                        		{section.title}
-                      		</Text>
-
-						)}/>
-
-            	</ScrollView>
-      
-				<Text>
-
-              		(C)2020 Globoticket
-
-        		</Text>
-
-		</View>
-	)
-	
-}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    height: 470,
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    backgroundColor: "rgba(247, 247, 247)",
+    borderBottomColor: "steelblue",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
